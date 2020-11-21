4c0dfd9566ad7d2b6be2c6026e540f39a48243c2
baylagas <balbino_a@hotmail.com>
2020-11-20 20:42:47 -0600

    another explanation of logic and databaseX

--- mainapp.py
+++ mainapp.py
@@ -1,45 +1,10 @@
-# from dx_logic import Logic
+from pepito_logic import PepitoLogic
 
-from user_logic import UserLogic
-from product_logic import ProductLogic
+pepitoObj = PepitoLogic()
+pepitoObjList = pepitoObj.getAllProducts()
 
-# logic = UserLogic()
-# userList = logic.getAllUsers()
-# for userObj in userList:
-#     print(userObj.id, userObj.user, userObj.password, userObj.email)
-
-# logic.insertUser("test02", "123", "test02@gmail.com")
-
-# logic.updateUserById(2, "test01x", "123", "test01x@gmail.com")
-
-# cc game fest
-# Python Invaders: Creando videojuegos con Python
-# miercoles 18 nov
-# 1:15pm - 1 hora
-
-
-# database = DatabaseX()
-# sql = "select * from ventafrutasplusdb.user;"
-# userList = database.executeQueryRows(sql)
-# for userDict in userList:
-#     print(userDict["user"], userDict["email"])
-
-# sql = "select * from ventafrutasplusdb.user where id=1;"
-# userDict = database.executeQueryOneRow(sql)
-# if userDict is not None:
-#     print(userDict["password"], userDict["user"])
-# else:
-#     print("el usuario no existe")
-
-# logic = UserLogic()
-
-logic = ProductLogic()
-
-# logic.insertProduct("kiwi", 2.50)
-
-# productList = logic.getAllProducts()
-# for productObj in productList:
-#     print(productObj.id, productObj.name, productObj.cost)
+for pepito in pepitoObjList:
+    print(pepito.id, pepito.name, pepito.cost)
 
 # import os
 
