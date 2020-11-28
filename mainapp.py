5db86cd615813f8dbc86392816d15d38a31e17c0
baylagas <balbino_a@hotmail.com>
2020-11-28 14:37:05 -0600

    modif sale_logic, mainapp, and sale_logic is finished

--- mainapp.py
+++ mainapp.py
@@ -1,53 +1,15 @@
-from cart_detail_logic import CartDetailLogic
-from product_logic import ProductLogic
-import random
+from sale_logic import SaleLogic
 
-prodLogic = ProductLogic()
+logic = SaleLogic()
+# logic.insertSale(3, 35.0, "3333444455556666")
 
+# logic.updateSaleById(3, 1, 25.00, "1111222233334444")
 
-def getRandomProduct(prodLogic):
-    randomProd = random.choice([1, 2, 3, 4, 5])
-    return prodLogic.getProductById(randomProd)
+# logic.deleteSaleById(4)
 
+saleObjList = logic.getAllSales()
+for saleObj in saleObjList:
+    print(saleObj.idUser, saleObj.total, saleObj.cardNumber)
 
-def getRandomeQuantity():
-    return random.choice([2, 3, 4, 5, 6])
-
-
-logic = CartDetailLogic()
-
-# # inserts
-# product = getRandomProduct(prodLogic)
-# quantity = getRandomeQuantity()
-# logic.insertCartDetail(2, product.id, product.cost, quantity, product.cost * quantity)
-# product = getRandomProduct(prodLogic)
-# quantity = getRandomeQuantity()
-# logic.insertCartDetail(2, product.id, product.cost, quantity, product.cost * quantity)
-# product = getRandomProduct(prodLogic)
-# quantity = getRandomeQuantity()
-# logic.insertCartDetail(2, product.id, product.cost, quantity, product.cost * quantity)
-
-# product = getRandomProduct(prodLogic)
-# quantity = getRandomeQuantity()
-# logic.insertCartDetail(5, product.id, product.cost, quantity, product.cost * quantity)
-# product = getRandomProduct(prodLogic)
-# quantity = getRandomeQuantity()
-# logic.insertCartDetail(5, product.id, product.cost, quantity, product.cost * quantity)
-
-# cdObjList = logic.getAllCartDetails()
-# for cdObj in cdObjList:
-#     print(cdObj.idProduct, cdObj.cost, cdObj.subtotal)
-
-# cdObj = logic.getCartDetailsById(3)
-# print(cdObj.idProduct, cdObj.cost, cdObj.subtotal)
-
-# logic.updateCartDetailById(3, 2, 5, 1.0, 5, 5.0)
-
-# product = getRandomProduct(prodLogic)
-# quantity = getRandomeQuantity()
-# logic.insertCartDetail(5, product.id, product.cost, quantity, product.cost * quantity)
-
-# logic.deleteCartDetailById(6)
-
-rows = logic.updateCartDetailByIdProduct(3, 4)
-print(f"{rows} affected")
+saleObj = logic.getSaleById(2)
+print(saleObj.idUser, saleObj.total, saleObj.cardNumber)
--- sale_logic.py
+++ sale_logic.py
@@ -1,4 +1,5 @@
 from dx_logic import Logic
+from sale_obj import SaleObj
 
 
 class SaleLogic(Logic):
@@ -17,3 +18,41 @@ class SaleLogic(Logic):
         rowDict = super().getRowById(id, self.tableName)
         newSale = self.createSaleObj(rowDict)
         return newSale
+
+    # polimorfismo
+    def createSaleObj(self, id, idUser, total, cardNumber):
+        saleObj = SaleObj(idUser, total, cardNumber, id)
+        return saleObj
+
+    def createSaleObj(self, saleDict):
+        saleObj = SaleObj(
+            saleDict["iduser"],
+            saleDict["total"],
+            saleDict["cardnumber"],
+            saleDict["id"],
+        )
+        return saleObj
+
+    def insertSale(self, idUser, total, cardNumber):
+        database = self.database
+        sql = (
+            f"INSERT INTO `ventafrutasplusdb`.`sale`"
+            + f"(`id`,`iduser`,`total`,`cardnumber`) "
+            + f"VALUES(0,{idUser},{total},'{cardNumber}');"
+        )
+        rows = database.executeNonQueryRows(sql)
+        return rows
+
+    def updateSaleById(self, id, idUser, total, cardNumber):
+        database = self.database
+        sql = (
+            "UPDATE `ventafrutasplusdb`.`sale` "
+            + f"SET `iduser` = {idUser}, `total` = {total}, "
+            + f"`cardnumber` = '{cardNumber}' "
+            + f"WHERE `id` = {id};"
+        )
+        rows = database.executeNonQueryRows(sql)
+        return rows
+
+    def deleteSaleById(self, id):
+        super().deleteRowById(id, self.tableName)
